8bc821346fd2625f16c01e9faec9eb79f43dd65f
diekleinekuh <markus.henschel@gmail.com>
2021-11-19 16:54:02 +0100

    add simple command to download dependencies and create teh docker image we use

---
+++ YaakBuildAgent.cmd
@@ -0,0 +1,9 @@
+pushd %~dp0
+curl.exe -L https://download.jetbrains.com/teamcity/TeamCity-2021.2.tar.gz --output - | tar xvz -f - -C context
+CALL .\generate.cmd
+
+pushd generated
+CALL .\teamcity-agent-local-windowsservercore-ltsc2022.cmd
+popd
+
+popd
